238f61b8b7d4ba2ced620c4ed02b409c9b577845
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-04-03 16:43:59 +0900

    コントローラー内にブロードキャストの機能を追加

--- app/controllers/websocket_chat_controller.rb
+++ app/controllers/websocket_chat_controller.rb
@@ -1,7 +1,11 @@
 class WebsocketChatController < WebsocketRails::BaseController
 
-  def message_recieve
+  def recieve_message
     # クライアントからメッセージを取得
+    recieve_message = message()
+
+    # websocket_chatイベントで接続しているクライアントにブロードキャスト(送信)
+    broadcast_message(:websocket_chat, recieve_message)
   end
 
 end
